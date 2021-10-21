79ed28331de312eb2ec7e917fc1dfc0af46744eb
Nikola <nikola.zivkovic300@gmail.com>
2021-10-21 07:20:20 +0200

    maison2

--- JS/dark_light.js
+++ JS/dark_light.js
@@ -1,15 +1,32 @@
 let darklight = document.getElementById("dark_light");
 darklight.addEventListener('click', appuiButton);
+let monBody = document.getElementById("body");
 
 function appuiButton()
 {
-  let monBody = document.getElementById("body");
-  if(monBody.hasAttribute("data-theme","light"))
+  if(monBody.getAttribute("data-theme")=="light")
   {
     monBody.setAttribute("data-theme","dark");
     darklight.innerText="Mode clair";
   }
 
+  else
+  {
+    monBody.setAttribute("data-theme","light");
+    darklight.innerText="Mode sombre";
+  }
+}
+
+const d = new Date();
+const hour = d.getHours();
+
+function darkLight()
+{
+  if(hour > 7 || hour < 19)
+  {
+    monBody.setAttribute("data-theme","dark");
+    darklight.innerText="Mode clair";
+  }
   else
   {
     monBody.setAttribute("data-theme","light");
